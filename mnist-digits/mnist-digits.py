0a1747e7200d61f2789d85687ff30f60ff3e2ab2
Jakub Adrian Niemiec <janiemiec20@gmail.com>
2018-09-06 17:24:36 +0100

    Added the mnist digits example cnn

---
+++ mnist-digits/mnist-digits.py
@@ -0,0 +1,59 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+import numpy as np
+
+batch_size = 128
+num_classes = 10
+epochs = 12
+
+# Dimensions of the input images
+img_rows, img_cols = 28, 28
+
+# Download the MNIST data into training and testing groups
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Reshape x because this CNN only accepts 4D vectors
+x_train = x_train.reshape(60000, 28, 28, 1) # 60K images, 28x28, with 1 channel
+x_test = x_test.reshape(10000, 28, 28, 1) # 10K images, 28x28, with 1 channel
+
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# Convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+# Build the sequential model with convolution and max pooling layers
+# Use dropout layers to randomly switch off some neurons forcing data to find new paths (reduce overfitting)
+# Adda dense layer for class prediction (0-9)
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=(28, 28, 1)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+
+# Compile the model with a categorical cross entropy loss function
+# Using Adadelta optimiser and an accuracy metric
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
