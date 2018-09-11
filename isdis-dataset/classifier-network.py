18b7db283bd2298576f2212fc31ca55e340d23ea
Jakub Adrian Niemiec <janiemiec20@gmail.com>
2018-09-11 14:38:34 +0100

    Annotated the main model Added GPU config support

--- isdis-dataset/classifier-network.py
+++ isdis-dataset/classifier-network.py
@@ -1,34 +1,48 @@
+import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.backend.tensorflow_backend import set_session
+
+# Network configuration to use GPUs
+config = tf.ConfigProto(
+    log_device_placement=True   # Show which device is being used
+    #gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=1.0)
+    #device_count = {'GPU': 1, 'CPU:': 8}
+)
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+set_session(session)
 
 
+# Build the sequential convolutional model for image classification
 model = Sequential()
 model.add(Conv2D(32, (3, 3), input_shape=(150, 150, 3), data_format='channels_last'))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
+# First convolutional layer
 model.add(Conv2D(32, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
+# Second convolutional layer
 model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-# the model so far outputs 3D feature maps (height, width, features)
-
-model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+# The model outputs 3D feature maps (height, width, features)
+# Fully connected layers
+model.add(Flatten())    # This converts 3D feature maps to 1D feature vectors
 model.add(Dense(64))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
 model.add(Dense(1))
-model.add(Activation('sigmoid'))
+model.add(Activation('sigmoid'))    # Sigmoid activation function
 
-model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
-              metrics=['accuracy'])
+# Using binary crossentropy loss for the model
+model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
 # Data Preparation
 batch_size = 16
@@ -48,14 +62,14 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 # subfolers of 'data/train', and indefinitely generate
 # batches of augmented image data
 train_generator = train_datagen.flow_from_directory(
-    '/home/niemtec/tmp/Project-Turing/isdis-dataset/train',  # this is the target directory
+    'C://Users//janie//Documents//GitHub//Project-Turing//isdis-dataset//train',  # this is the target directory
     target_size=(150, 150),  # all images will be resized to 150x150
     batch_size=batch_size,
     class_mode='binary')  # since we use binary_crossentropy loss, we need binary labels
 
 # this is a similar generator, for validation data
 validation_generator = test_datagen.flow_from_directory(
-    '/home/niemtec/tmp/Project-Turing/isdis-dataset/validation',
+    'C://Users//janie//Documents//GitHub//Project-Turing//isdis-dataset//validation',
     target_size=(150, 150),
     batch_size=batch_size,
     class_mode='binary')
@@ -67,4 +81,4 @@ model.fit_generator(
     validation_data=validation_generator,
     validation_steps=800 // batch_size)
 
-model.save_weights('/home/niemtec/tmp/Project-Turing/isdis-dataset/first_try.h5')  # always save your weights after training or during training
+model.save_weights('C://Users//janie//Documents//GitHub//Project-Turing//isdis-dataset//first_try.h5')  # always save your weights after training or during training
