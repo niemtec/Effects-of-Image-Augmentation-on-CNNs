adca841ffa01910263fcf4af7fa756abd21c5168
Jakub Adrian Niemiec <niemtec@users.noreply.github.com>
2018-09-27 16:08:12 +0100

    Converted padding into a function for recursive use

--- tools/image-padder.py
+++ tools/image-padder.py
@@ -1,24 +1,33 @@
 # Script used to add padding to dataset images for input standarisation
+import os
+
 from PIL import Image, ImageOps
 
-desired_size = 1024
-im_path = 'C://Users//janie//PycharmProjects//Project-Turing//train//benign//ISIC_0000003.jpg'
 
-im = Image.open(im_path)
-old_size = im.size # old_size[0] is in (width, height) format
+def padImage(path_to_image, path_to_processed_directory):
+    desired_size = 1024
+    im_path = path_to_image
 
-ratio = float(desired_size) / max(old_size)
-new_size = tuple([int(x*ratio) for x in old_size])
-# use thumbnail() or resize() method to resize the input image
-# thumbnail is a in-place operation
+    im = Image.open(im_path)
+    old_size = im.size # old_size[0] is in (width, height) format
 
-# im.thumbnail(new_size, Image.ANTIALIAS)
+    ratio = float(desired_size) / max(old_size)
+    new_size = tuple([int(x*ratio) for x in old_size])
+    # use thumbnail() or resize() method to resize the input image
+    # thumbnail is a in-place operation
 
-im = im.resize(new_size, Image.ANTIALIAS)
-# Create a  new i mage and paste the resized onto it
-new_im = Image.new("RGB", (desired_size, desired_size))
-new_im.paste(im, ((desired_size-new_size[0])//2,
+    # im.thumbnail(new_size, Image.ANTIALIAS)
+
+    im = im.resize(new_size, Image.ANTIALIAS)
+    # Create a  new i mage and paste the resized onto it
+    new_im = Image.new("RGB", (desired_size, desired_size))
+    new_im.paste(im, ((desired_size-new_size[0])//2,
                     (desired_size-new_size[1])//2))
 
-new_im.show()
-new_im.save("C://Users//janie//PycharmProjects//Project-Turing//sample.png", "PNG")
+    new_im.show()
+    path, new_filename = os.path.split(path_to_image)
+
+    new_im.save((path_to_processed_directory + "//" + new_filename), "JPG")
+
+padImage("C://Users//janie//PycharmProjects//Project-Turing//train//benign//ISIC_0000003.jpg",
+             "C://Users//janie//PycharmProjects//Project-Turing")
