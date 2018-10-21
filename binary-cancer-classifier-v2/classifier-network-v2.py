28e3450d3760a8efd15a19235bf7be7cbce9f6f4
Jakub Adrian Niemiec <niemtec@users.noreply.github.com>
2018-10-21 16:51:48 +0100

    Annotated the classifier network

--- binary-cancer-classifier-v2/classifier-network-v2.py
+++ binary-cancer-classifier-v2/classifier-network-v2.py
@@ -2,11 +2,11 @@ from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
-
-input_shape = (150, 150, 3)
-nClasses = 2
-batch_size = 30
-epochs = 100
+# Control Variables
+input_shape = (150, 150, 3)     # Input shape of the images (H x W x D)
+nClasses = 2    # Number of classes for binary classification
+batch_size = 540     # Number of samples to present to the network
+epochs = 100    # Number of epochs to run for
 training_directory = 'C://Users//janie//PycharmProjects//Project-Turing//train'
 validation_directory = 'C://Users//janie//PycharmProjects//Project-Turing//validation'
 
@@ -32,26 +32,27 @@ def CreateModel():
     model.add(Dropout(0.25))
 
     # Fully connected layers
-    model.add(Flatten())
+    model.add(Flatten())    # This converts 3D feature maps to 1D feature vectors
     model.add(Dense(512, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(nClasses, activation='softmax')) # Using softmax instead of sigmoid
+    model.add(Dense(nClasses, activation='softmax'))    # Using softmax instead of sigmoid
 
     return model
 
+# Load training dataset
 train_datagen = ImageDataGenerator()
-
 train_data = train_datagen.flow_from_directory(training_directory, target_size=(150, 150), batch_size=batch_size, class_mode='binary')
 
+# Load validation dataset
 validation_datagen = ImageDataGenerator
-
 validation_data = validation_datagen.flow_from_directory(validation_directory, target_size=(150, 150), batch_size=batch_size, class_mode='binary')
 
-
+# Build the network model
 model1 = CreateModel()
-
+# Using binary crossentropy loss for the model
 model1.compile(optimiser='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
+# Save the history of model fitting
 history = model1.fit(train_data, train_labels, batch_size=batch_size, epochs=epochs, verbose = 1, validation_data=(test_data, test_labels))
 
 model1.evaluate(test_data, test_labels)
