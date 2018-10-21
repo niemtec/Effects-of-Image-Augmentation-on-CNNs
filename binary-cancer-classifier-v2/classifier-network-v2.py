d9f44fb0fa0453fd3b1a011fb10cf9c6b5b88e19
Jakub Adrian Niemiec <niemtec@users.noreply.github.com>
2018-10-21 16:59:33 +0100

    Added plotting library and calls

--- binary-cancer-classifier-v2/classifier-network-v2.py
+++ binary-cancer-classifier-v2/classifier-network-v2.py
@@ -1,6 +1,8 @@
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+import matplotlib.pyplot as plt
+
 
 # Control Variables
 input_shape = (150, 150, 3)     # Input shape of the images (H x W x D)
@@ -53,6 +55,44 @@ model1 = CreateModel()
 model1.compile(optimiser='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Save the history of model fitting
-history = model1.fit(train_data, train_labels, batch_size=batch_size, epochs=epochs, verbose = 1, validation_data=(test_data, test_labels))
+history = model1.fit_generator(train_data, batch_size=batch_size, epochs=epochs, verbose = 1, validation_data=validation_data)
 
-model1.evaluate(test_data, test_labels)
+model1.evaluate_generator(validation_data)
+
+# Loss Curves
+plt.figure(figsize=[8, 6])
+plt.plot(history.history['loss'], 'r', linewidth=3.0)
+plt.plot(history.history['val_loss'], 'b', linewidth=3.0)
+plt.legend(['Training loss', 'Validation Loss'], fontsize=18)
+plt.xlabel('Epochs ', fontsize=16)
+plt.ylabel('Loss', fontsize=16)
+plt.title('Loss Curves', fontsize=16)
+
+# Accuracy Curves
+plt.figure(figsize=[8, 6])
+plt.plot(history.history['acc'], 'r', linewidth=3.0)
+plt.plot(history.history['val_acc'], 'b', linewidth=3.0)
+plt.legend(['Training Accuracy', 'Validation Accuracy'], fontsize=18)
+plt.xlabel('Epochs ', fontsize=16)
+plt.ylabel('Accuracy', fontsize=16)
+plt.title('Accuracy Curves', fontsize=16)
+
+# list all data in history
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
