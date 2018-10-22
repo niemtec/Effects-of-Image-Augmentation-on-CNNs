087eae0bc46e5be70bbef7b12d1ca2d7f6fb633c
Jakub Adrian Niemiec <janiemiec20@gmail.com>
2018-10-22 13:55:12 +0100

    Removed redundant graphs

--- binary-cancer-classifier-v2/classifier-network-v2.py
+++ binary-cancer-classifier-v2/classifier-network-v2.py
@@ -72,27 +72,6 @@ history = model.fit_generator(
     validation_steps=800 // batch_size)
 
 model.evaluate_generator(validation_generator, steps=number_of_evaluation_samples // batch_size)
-
-# Loss Curves
-plt.figure(figsize=[8, 6])
-plt.plot(history.history['loss'], 'r', linewidth=3.0)
-plt.plot(history.history['val_loss'], 'b', linewidth=3.0)
-plt.legend(['Training loss', 'Validation Loss'], fontsize=18)
-plt.xlabel('Epochs ', fontsize=16)
-plt.ylabel('Loss', fontsize=16)
-plt.title('Loss Curves', fontsize=16)
-plt.show()
-
-# Accuracy Curves
-plt.figure(figsize=[8, 6])
-plt.plot(history.history['acc'], 'r', linewidth=3.0)
-plt.plot(history.history['val_acc'], 'b', linewidth=3.0)
-plt.legend(['Training Accuracy', 'Validation Accuracy'], fontsize=18)
-plt.xlabel('Epochs ', fontsize=16)
-plt.ylabel('Accuracy', fontsize=16)
-plt.title('Accuracy Curves', fontsize=16)
-plt.show()
-
 # list all data in history
 print(history.history.keys())
 # summarize history for accuracy
