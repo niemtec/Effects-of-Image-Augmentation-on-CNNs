e3cf26bcc234ef44a7edbb38d19904ce826eb4a5
Jakub Adrian Niemiec <niemtec@users.noreply.github.com>
2018-10-25 15:01:15 +0100

    Added generators and model checkpoint saving

--- convNet/convNet.py
+++ convNet/convNet.py
@@ -18,47 +18,63 @@ batch_size = 16
 epochs = 50
 
 model = applications.VGG19(
-    weights='imagenet',
-    include_top=False,
-    input_shape=(img_width, img_height, 3))
+   weights = 'imagenet',
+   include_top = False,
+   input_shape = (img_width, img_height, 3))
 
 # Freeze the layers which you don't want to train
 for layer in model.layers[:5]:
-    layer.trainable = False
+   layer.trainable = False
 
 # Adding custom layers
 x = model.output
 x = Flatten()(x)
-x = Dense(1024, activation='relu')(x)
+x = Dense(1024, activation = 'relu')(x)
 x = Dropout(0.5)(x)
-x = Dense(1024, activation='relu')(x)
-predictions = Dense(16, activation='softmax')(x)
+x = Dense(1024, activation = 'relu')(x)
+predictions = Dense(16, activation = 'softmax')(x)
 
 # Creating the final model
 model_final = Model(
-    input=model.input,
-    output=predictions)
+   input = model.input,
+   output = predictions)
 
 # Compile the model
 model_final.compile(
-    loss='binary_crossentropy',
-    optimizer=optimizers.SGD(lr=0.0001, momentum=0.9),
-    metrics=['accuracy'])
+   loss = 'binary_crossentropy',
+   optimizer = optimizers.SGD(lr = 0.0001, momentum = 0.9),
+   metrics = ['accuracy'])
 
 # Initiate the train and test generators with data Augmentation
 train_datagen = ImageDataGenerator(
-    rescale=1./255,
-    horizontal_flip=True,
-    vertical_flip=True,
-    fill_mode='nearest',
-    zoom_range=0.3,
-    rotation_range=30)
+   rescale = 1. / 255,
+   horizontal_flip = True,
+   vertical_flip = True,
+   fill_mode = 'nearest',
+   zoom_range = 0.3,
+   rotation_range = 30)
 
-test_datagen = ImageDataGenerator(
-    rescale=1./255,
-    horizontal_flip=True,
-    vertical_flip=True,
-    fill_mode='nearest',
-    zoom_range=0.3,
-    rotation_range=30)
+validation_datagen = ImageDataGenerator(
+   rescale = 1. / 255,
+   horizontal_flip = True,
+   vertical_flip = True,
+   fill_mode = 'nearest',
+   zoom_range = 0.3,
+   rotation_range = 30)
+
+train_generator = train_datagen.flow_from_directory(
+   train_data_dir,
+   target_size = (img_height, img_width),
+   batch_size = batch_size,
+   class_mode = 'binary')
+
+validation_generator = validation_datagen.flow_from_directory(
+   validation_data_dir,
+   target_size = (img_height, img_width),
+   batch_size = batch_size,
+   class_mode = 'binary')
+
+# Save the model according to the conditions
+checkpoint = ModelCheckpoint("convNet/vgg16_1.h5", monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
+early = EarlyStopping(monitor='convNet/val_acc', min_delta=0, patience=10, verbose=1, mode='auto')
 
