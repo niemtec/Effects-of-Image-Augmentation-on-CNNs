d339e2e58f07e9ec3c93e58a7f88d611334b8e34
Jakub Adrian Niemiec <niemtec@users.noreply.github.com>
2018-10-25 14:50:28 +0100

    Imported existing network and added custom layers

--- convNet/convNet.py
+++ convNet/convNet.py
@@ -8,3 +8,30 @@ from keras.layers import Dropout, Flatten, Dense, GlobalAveragePooling2D
 from keras import backend as k
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler, TensorBoard, EarlyStopping
 
+img_width = 256
+img_height = 256
+train_data_dir = 'padded-dataset/train'
+validation_data_dir = 'padded-dataset/validation'
+nb_train_samples = 512
+nb_validation_samples = 512
+batch_size = 16
+epochs = 50
+
+model = applications.VGG19(
+    weights = 'imagenet',
+    include_top = False,
+    input_shape = (img_width, img_height, 3))
+
+# Freeze the layers which you don't want to train
+for layer in model.layers[:5]:
+    layer.trainable = False
+
+# Adding custom layers
+x = model.output
+x = Flatten()(x)
+x = Dense(1024, activation='relu')(x)
+x = Dropout(0.5)(x)
+x = Dense(1024, activation='relu')(x)
+predictions = Dense(16, activation='softmax')(x)
+
+# Creating the final model
