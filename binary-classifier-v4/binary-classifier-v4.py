b1bb97bbd1c24ac734589d69af4564770ef5743f
Jake A. Niemiec <janiemiec20@gmail.com>
2018-12-21 14:14:13 +0000

    Renamed model filename for consistency

---
+++ binary-classifier-v4/binary-classifier-v4.py
@@ -0,0 +1,197 @@
+import matplotlib
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import img_to_array
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Flatten
+from keras.layers.core import Dense
+from keras import backend as K
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import cv2
+import os
+
+# Set the matplotlib backend so figures can be saved in the background
+matplotlib.use("Agg")
+
+
+# Determine whether given file is an image or not
+def file_is_image(path_to_file):
+    filename, extension = os.path.splitext(path_to_file)
+    if extension != '.jpg':
+        return False
+    else:
+        return True
+
+
+# Build the network structure
+def build_lenet_model(width, height, depth, classes):
+    # Initialise the model
+    model = Sequential()
+    inputShape = (height, width, depth)
+
+    # If 'channel first' is being used, update the input shape
+    if K.image_data_format() == 'channel_first':
+        inputShape = (depth, height, width)
+
+    # Model Structure
+    # First layer | CONV > RELU > POOL
+    model.add(
+        Conv2D(20, (5, 5), padding = "same", input_shape = inputShape))  # Learning 20 (5 x 5) convolution filters
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size = (2, 2), strides = (2, 2)))
+
+    # Second layer | CONV > RELU > POOL
+    model.add(Conv2D(50, (5, 5), padding = "same"))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size = (2, 2), strides = (2, 2)))
+
+    # Third layer | flattening out into fully-connected layers
+    model.add(Flatten())
+    model.add(Dense(500))  # 500 nodes
+    model.add(Activation("relu"))
+
+    # Softmax classifier
+    model.add(Dense(classes))  # number of nodes = number of classes
+    model.add(Activation("softmax"))  # yields probability for each class
+
+    # Return the model
+    return model
+
+
+# Control Variables
+modelName = 'classifier-v4-cancer-dataset-no-augmentation'
+datasetPath = '../datasets/isic/train'
+plotName = modelName
+graphSize = (15, 10)  # Size of result plots
+
+noEpochs = 300
+initialLearningRate = 1e-3
+batchSize = 32
+decayRate = initialLearningRate / noEpochs
+
+numberOfClasses = 2
+categoryOne = 'benign'
+categoryTwo = 'malignant'
+testDatasetSize = 0.25  # Using 75% of the data for training and the remaining 25% for testing
+randomSeed = 42  # For repeatability
+imageHeight = 28
+imageWidth = 28
+imageDepth = 3
+
+# Initialize the data and labels arrays
+sortedData = []
+sortedLabels = []
+data = []
+labels = []
+
+# Go through dataset directory
+print("Classifying the Dataset")
+for datasetCategory in os.listdir(datasetPath):
+    datasetCategoryPath = datasetPath + "/" + datasetCategory
+
+    # Go through category 1 and then category 2 of the dataset
+    for sample in os.listdir(datasetCategoryPath):
+        print(sample)
+        if file_is_image(datasetCategoryPath + "/" + sample):
+            image = cv2.imread(datasetCategoryPath + "/" + sample)
+            image = cv2.resize(image, (28, 28))  # Network only accepts 28 x 28 so resize the image accordingly
+            image = img_to_array(image)
+            # Save image to the data list
+            sortedData.append(image)
+
+            # Decide on binary label
+            if datasetCategory == categoryOne:
+                label = 1
+            else:
+                label = 0
+            # Save label for the current image
+            sortedLabels.append(label)
+
+combined = list(zip(sortedData, sortedLabels))
+random.shuffle(combined)
+data[:], labels[:] = zip(*combined)
+
+# Scale the raw pixel intensities to the range [0, 1]
+data = np.array(data, dtype = "float") / 255.0
+labels = np.array(labels)
+
+# Partition the data into training and testing splits
+(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size = testDatasetSize, random_state = randomSeed)
+
+# Convert the labels from integers to vectors
+trainY = to_categorical(trainY, num_classes = numberOfClasses)
+testY = to_categorical(testY, num_classes = numberOfClasses)
+
+# TODO: Check results with these parameters disabled
+# Construct the image generator for data augmentation
+aug = ImageDataGenerator(
+    # rotation_range = 90,
+    # width_shift_range = 0.1,
+    # height_shift_range = 0.1,
+    # shear_range = 0.2,
+    # zoom_range = 0.2,
+    horizontal_flip = True,
+    fill_mode = "nearest"
+)
+
+# Initialize the model
+print("Compiling Network Model")
+model = build_lenet_model(width = imageWidth, height = imageHeight, depth = imageDepth, classes = numberOfClasses)
+opt = Adam(lr = initialLearningRate, decay = decayRate)
+model.compile(loss = "binary_crossentropy", optimizer = opt, metrics = ["accuracy"])
+
+# Train the network
+print("Training Network Model")
+history = model.fit_generator(aug.flow(trainX, trainY, batch_size = batchSize), validation_data = (testX, testY),
+                              steps_per_epoch = len(trainX) // batchSize, epochs = noEpochs, verbose = 1)
+
+# Save the model to disk
+print("Saving Network Model")
+model_json = model.to_json()
+with open(modelName + ".json", "w") as json_file:
+    json_file.write(model_json)
+
+# # plot the training loss and accuracy
+# plt.style.use("ggplot")
+# plt.figure()
+# N = noEpochs
+# plt.plot(np.arange(0, N), history.history["loss"], label = "train_loss")
+# plt.plot(np.arange(0, N), history.history["val_loss"], label = "val_loss")
+# plt.plot(np.arange(0, N), history.history["acc"], label = "train_acc")
+# plt.plot(np.arange(0, N), history.history["val_acc"], label = "val_acc")
+# plt.title("Training Loss and Accuracy on Santa/Not Santa")
+# plt.xlabel("Epoch #")
+# plt.ylabel("Loss/Accuracy")
+# plt.legend(loc = "lower left")
+# plt.savefig(plotName)
+
+# Summarize history for accuracy
+plt.figure(figsize = graphSize, dpi = 75)
+plt.grid(True, which = 'both')
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc = 'upper left')
+plt.savefig('Results/' + modelName + "-accuracy.png")
+plt.close()
+
+# Summarize history for loss
+plt.figure(figsize = graphSize, dpi = 75)
+plt.grid(True, which = 'both')
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc = 'upper left')
+plt.savefig('Results/' + modelName + "-loss.png")
+plt.close()
