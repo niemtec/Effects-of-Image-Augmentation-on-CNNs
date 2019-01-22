6cc0870b4f6c339649b1033a1363a8a3452577da
Jake A. Niemiec <janiemiec20@gmail.com>
2019-01-22 14:30:36 +0000

    Added image loader and plotter functions

--- dataset-image-printer.py
+++ dataset-image-printer.py
@@ -1,1 +1,23 @@
-# Tools used to print the dataset images for pictographic representation of data augmentation techniques
+# Tool used to print the dataset images for pictographic representation of data augmentation techniques
+from keras.preprocessing.image import img_to_array
+import matplotlib.pyplot as plt
+import cv2
+
+# Control Variables
+imageHeight = 28
+imageWidth = 28
+
+
+def load_image(pathToImage):
+    # Load the image
+    image = cv2.imread(pathToImage)
+    # Resize the image
+    image = cv2.resize(image, imageHeight, imageWidth)
+    # Convert image to array
+    imageArray = img_to_array(image)
+
+
+def plot_image(imageArray, saveDirectory, newImageName):
+    # DEBUG: Show image if you want
+    plt.imshow(imageArray / 255)  # /255 to make image coloured again
+    plt.savefit(saveDirectory + "/" + newImageName + ".png")
