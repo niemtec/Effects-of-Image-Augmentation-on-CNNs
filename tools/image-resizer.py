2946cb955aca64f6aa7c9227348f12e4b88c37ed
Jake A. Niemiec <janiemiec20@gmail.com>
2019-02-18 12:54:20 +0000

    Updated image resizer to force set dimensions

--- tools/image-resizer.py
+++ tools/image-resizer.py
@@ -4,18 +4,19 @@ import os
 from PIL import Image, ImageOps
 
 def resizeImage(path_to_image, path_to_processed_directory):
-    desired_size = 512, 512
+    desired_size = (64, 64)
 
-    im = Image.open(path_to_image)
-    im.thumbnail(desired_size, Image.ANTIALIAS)
+    original_image = Image.open(path_to_image)
+    new_image = ImageOps.fit(original_image, desired_size, Image.ANTIALIAS)
     path, new_filename = os.path.split(path_to_image)
 
-    im.save((path_to_processed_directory + "//" + new_filename), "PNG")
+    new_image.save((path_to_processed_directory + "//" + new_filename), "PNG")
 
-working_directory = "padded-dataset//validation//benign"
+working_directory = "datasets/cats-dogs/cat"
 
 for file in os.listdir(working_directory):
     print(file)
-    output_path = "rescaled-dataset-512//validation//benign"
-    resizeImage(working_directory + "//" + file, output_path)
+    output_path = "datasets/cats-dogs-resized/cat"
+    if(file != "desktop.ini"):
+        resizeImage(working_directory + "//" + file, output_path)
 
