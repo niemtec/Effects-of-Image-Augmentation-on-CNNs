3d8a73f2154cca46c8edb2beebdd6f4a48f9e04f
Jake A. Niemiec <janiemiec20@gmail.com>
2019-02-28 13:59:43 +0000

    Training Rotation 180

--- binary-classifier-network.py
+++ binary-classifier-network.py
@@ -29,8 +29,8 @@ matplotlib.use("Agg")
 home = os.environ['HOME']
 datasetName = 'control'
 resultsFileName = 'cats-dogs-rotation-training'
-rotationRange = 170
-modelName = 'training-rotation-170'
+rotationRange = 180
+modelName = 'training-rotation-180'
 datasetPath = home + '/home/Downloads/Project-Turing/datasets/cats-dogs-resized/cats-dogs-noise-000/'
 resultsPath = home + '/home/Downloads/Project-Turing/results/rotation-experiments/' + resultsFileName + '/'
 plotName = modelName
