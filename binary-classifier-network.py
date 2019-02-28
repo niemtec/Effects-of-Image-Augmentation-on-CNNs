7d839e3737f5792c00f841e62adc2dc7026582e8
Jake A. Niemiec <janiemiec20@gmail.com>
2019-02-28 14:20:28 +0000

    Validation Cancer 110

--- binary-classifier-network.py
+++ binary-classifier-network.py
@@ -29,8 +29,8 @@ matplotlib.use("Agg")
 home = os.environ['HOME']
 datasetName = 'control'
 resultsFileName = 'cancer-rotation-validation'
-rotationRange = 100
-modelName = 'validation-rotation-100'
+rotationRange = 110
+modelName = 'validation-rotation-110'
 datasetPath = home + '/home/Downloads/Project-Turing/datasets/isic-resized/'
 resultsPath = home + '/home/Downloads/Project-Turing/results/rotation-experiments/' + resultsFileName + '/'
 plotName = modelName
