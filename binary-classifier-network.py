0170d746458a7c4b2e755bb8d582987ce72b588d
Jake A. Niemiec <janiemiec20@gmail.com>
2019-02-28 14:03:59 +0000

    Validation Rotation 30

--- binary-classifier-network.py
+++ binary-classifier-network.py
@@ -29,8 +29,8 @@ matplotlib.use("Agg")
 home = os.environ['HOME']
 datasetName = 'control'
 resultsFileName = 'cats-dogs-rotation-validation'
-rotationRange = 20
-modelName = 'validation-rotation-20'
+rotationRange = 30
+modelName = 'validation-rotation-30'
 datasetPath = home + '/home/Downloads/Project-Turing/datasets/cats-dogs-resized/cats-dogs-noise-000/'
 resultsPath = home + '/home/Downloads/Project-Turing/results/rotation-experiments/' + resultsFileName + '/'
 plotName = modelName
