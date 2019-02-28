f442c6ee16ae07320e232848d5743bed8f7340a1
Jake A. Niemiec <janiemiec20@gmail.com>
2019-02-28 13:33:20 +0000

    Animal Rotation 120

--- binary-classifier-network.py
+++ binary-classifier-network.py
@@ -28,8 +28,8 @@ matplotlib.use("Agg")
 # Control Variables
 home = os.environ['HOME']
 datasetName = 'control'
-resultsFileName = 'cats-dogs-rotation-110'
-rotationRange = 110
+resultsFileName = 'cats-dogs-rotation-120'
+rotationRange = 120
 modelName = '' + datasetName
 datasetPath = home + '/home/Downloads/Project-Turing/datasets/cats-dogs-resized/cats-dogs-noise-000/'
 resultsPath = home + '/home/Downloads/Project-Turing/results/rotation-experiments/' + resultsFileName + '/'
