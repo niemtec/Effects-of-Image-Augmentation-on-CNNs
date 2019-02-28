8ea184b70082a5306e4557d9f37ed5588da5bc69
Jake A. Niemiec <janiemiec20@gmail.com>
2019-02-28 14:08:33 +0000

    All Rotation 90

--- binary-classifier-network.py
+++ binary-classifier-network.py
@@ -29,8 +29,8 @@ matplotlib.use("Agg")
 home = os.environ['HOME']
 datasetName = 'control'
 resultsFileName = 'cats-dogs-rotation-all'
-rotationRange = 80
-modelName = 'all-rotation-80'
+rotationRange = 90
+modelName = 'all-rotation-90'
 datasetPath = home + '/home/Downloads/Project-Turing/datasets/cats-dogs-resized/cats-dogs-noise-000/'
 resultsPath = home + '/home/Downloads/Project-Turing/results/rotation-experiments/' + resultsFileName + '/'
 plotName = modelName
