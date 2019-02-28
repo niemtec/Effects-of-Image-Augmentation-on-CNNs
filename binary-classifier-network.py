a45b2ed6e8872c63723966011c80e0b26b002053
Jake A. Niemiec <janiemiec20@gmail.com>
2019-02-28 13:57:44 +0000

    Training Rotation 40

--- binary-classifier-network.py
+++ binary-classifier-network.py
@@ -29,8 +29,8 @@ matplotlib.use("Agg")
 home = os.environ['HOME']
 datasetName = 'control'
 resultsFileName = 'cats-dogs-rotation-training'
-rotationRange = 30
-modelName = 'training-rotation-30'
+rotationRange = 40
+modelName = 'training-rotation-40'
 datasetPath = home + '/home/Downloads/Project-Turing/datasets/cats-dogs-resized/cats-dogs-noise-000/'
 resultsPath = home + '/home/Downloads/Project-Turing/results/rotation-experiments/' + resultsFileName + '/'
 plotName = modelName
