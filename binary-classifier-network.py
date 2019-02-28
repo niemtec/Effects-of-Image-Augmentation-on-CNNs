c22e8cee2e4e9d4aad94b87733f0f2d9f8c4bb91
Jake A. Niemiec <janiemiec20@gmail.com>
2019-02-28 14:11:22 +0000

    All Cancer 70

--- binary-classifier-network.py
+++ binary-classifier-network.py
@@ -29,8 +29,8 @@ matplotlib.use("Agg")
 home = os.environ['HOME']
 datasetName = 'control'
 resultsFileName = 'cancer-rotation-all'
-rotationRange = 60
-modelName = 'all-rotation-60'
+rotationRange = 70
+modelName = 'all-rotation-70'
 datasetPath = home + '/home/Downloads/Project-Turing/datasets/isic-resized/'
 resultsPath = home + '/home/Downloads/Project-Turing/results/rotation-experiments/' + resultsFileName + '/'
 plotName = modelName
