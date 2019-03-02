7caaa78714ff02121ee52aa4c12b895a3085f846
Jake A. Niemiec <janiemiec20@gmail.com>
2019-03-02 10:37:14 +0000

    Cancer Noise 010 - Training Corrupted

--- binary-classifier-network.py
+++ binary-classifier-network.py
@@ -27,7 +27,7 @@ matplotlib.use("Agg")
 
 # Control Variables
 home = os.environ['HOME']
-experimentVariantDatasetName = 'control'
+experimentVariantDatasetName = 'training-corrupted'
 resultsFileName = 'cancer-noise-010-results'
 modelName = 'cancer-noise-010-' + experimentVariantDatasetName
 datasetPath = home + '/home/Downloads/Project-Turing/datasets/image-corruption-dataset/cancer-noise-010'
