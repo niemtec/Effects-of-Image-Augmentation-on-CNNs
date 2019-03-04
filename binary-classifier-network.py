eed170241986b38c6219513e2a59758f4f12ea3e
Jake A. Niemiec <janiemiec20@gmail.com>
2019-03-04 17:50:25 +0000

    Cancer - 10 - Control

--- binary-classifier-network.py
+++ binary-classifier-network.py
@@ -27,7 +27,7 @@ matplotlib.use("Agg")
 
 # Control Variables
 home = os.environ['HOME']
-experimentVariantDatasetName = 'validation-corrupted'
+experimentVariantDatasetName = 'control'
 resultsFileName = 'cancer-noise-010-results'
 modelName = 'cancer-noise-010-' + experimentVariantDatasetName
 datasetPath = home + '/home/Downloads/Project-Turing/datasets/image-corruption-dataset/cancer-noise-010'
