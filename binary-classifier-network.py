a38b5d933031ec0ab5da2a437e42c0435fb587cb
Jake A. Niemiec <janiemiec20@gmail.com>
2019-03-06 17:22:48 +0000

    Animal - 5 - Training Corrupted

--- binary-classifier-network.py
+++ binary-classifier-network.py
@@ -27,7 +27,7 @@ matplotlib.use("Agg")
 
 # Control Variables
 home = os.environ['HOME']
-experimentVariantDatasetName = 'all-corrupted'
+experimentVariantDatasetName = 'training-corrupted'
 resultsFileName = 'animal-noise-005-results'
 modelName = 'animal-noise-005-' + experimentVariantDatasetName
 categoryOne = 'dog'
