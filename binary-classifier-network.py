a059d46b4f02e2f8805a0c540d37943c58d09e72
Jake A. Niemiec <janiemiec20@gmail.com>
2019-03-06 11:54:18 +0000

    Cancer - 5 - Trainng Corrupted

--- binary-classifier-network.py
+++ binary-classifier-network.py
@@ -27,7 +27,7 @@ matplotlib.use("Agg")
 
 # Control Variables
 home = os.environ['HOME']
-experimentVariantDatasetName = 'validation-corrupted'
+experimentVariantDatasetName = 'training-corrupted'
 resultsFileName = 'cancer-noise-005-results'
 modelName = 'cancer-noise-005-' + experimentVariantDatasetName
 categoryOne = 'benign'
