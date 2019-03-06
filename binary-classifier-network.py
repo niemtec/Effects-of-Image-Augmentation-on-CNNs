6e98fab7856d7afd52df0ae01db6e2cf4a795399
Jake A. Niemiec <janiemiec20@gmail.com>
2019-03-06 17:14:59 +0000

    Cancer - 15 - All Corrupted

--- binary-classifier-network.py
+++ binary-classifier-network.py
@@ -28,11 +28,11 @@ matplotlib.use("Agg")
 # Control Variables
 home = os.environ['HOME']
 experimentVariantDatasetName = 'all-corrupted'
-resultsFileName = 'cancer-noise-005-results'
-modelName = 'cancer-noise-005-' + experimentVariantDatasetName
+resultsFileName = 'cancer-noise-015-results'
+modelName = 'cancer-noise-015-' + experimentVariantDatasetName
 categoryOne = 'benign'
 categoryTwo = 'malignant'
-datasetPath = home + '/home/Downloads/Project-Turing/datasets/image-corruption-dataset/cancer-noise-005'
+datasetPath = home + '/home/Downloads/Project-Turing/datasets/image-corruption-dataset/cancer-noise-015'
 resultsPath = home + '/home/Downloads/Project-Turing/results/cancer-noise-experiments/'
 plotName = modelName
 graphSize = (15, 10)  # Size of result plots
