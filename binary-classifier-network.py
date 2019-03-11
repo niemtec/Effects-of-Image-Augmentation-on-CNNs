c3b855703db8fe109275450c043b20a41ee0a100
Jake A. Niemiec <janiemiec20@gmail.com>
2019-03-11 14:27:55 +0000

    Heatmap - All Corrupted - 10

--- binary-classifier-network.py
+++ binary-classifier-network.py
@@ -29,10 +29,10 @@ matplotlib.use("Agg")
 home = os.environ['HOME']
 experimentVariantDatasetName = 'all-corrupted'
 resultsFileName = 'cancer-heatmap-all-corrupted'
-modelName = 'cancer-noise-005-' + experimentVariantDatasetName
+modelName = 'cancer-noise-010-' + experimentVariantDatasetName
 categoryOne = 'benign'
 categoryTwo = 'malignant'
-datasetPath = home + '/home/Downloads/Project-Turing/datasets/image-corruption-dataset/cancer-noise-005/'
+datasetPath = home + '/home/Downloads/Project-Turing/datasets/image-corruption-dataset/cancer-noise-010/'
 resultsPath = home + '/home/Downloads/Project-Turing/results/cancer-noise-experiments/heatmaps/'
 plotName = modelName
 graphSize = (15, 10)  # Size of result plots
