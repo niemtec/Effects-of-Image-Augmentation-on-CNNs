aa9fad2b6ae35e4d7a959fe752ecb2c26338a2e5
Jake <janiemiec20@gmail.com>
2019-06-16 13:42:32 +0100

    Annotated the main classifier script

--- demo/classifier-noise.py
+++ demo/classifier-noise.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python
+
+"""
+Experiment setup script for investigating the phenomena of overfitting in Convolutional Neural Networks
+using binary classification of cancer images. Model based on a modified LeNet architecture focussed on 
+binary classification of samples.
+
+This project has been put together as part of a dissertation in BSc (Hons) Computer Science with Artificial Intelligence.
+"""
+
+__author__ = "Jakub Adrian Niemiec"
+
+
 from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import Adam
 from sklearn.metrics import confusion_matrix
@@ -19,36 +32,33 @@ import random
 import cv2
 import os
 
-# BINARY CLASSIFIER NETWORK BASED ON THE LENET MODEL BUILT FOR IMAGE CLASSIFICATION TASKS
-# PART OF 'PROJECT TURING' - JAKUB ADRIAN NIEMIEC (@niemtec)
-# THIS MODEL IS USED AS A TOOL FOR INVESTIGATING THE PHENOMENA OF OVERFITTING IN CONVOLUTIONAL NEURAL NETWORKS
 
 # Set the matplotlib backend so figures can be saved in the background
 matplotlib.use("Agg")
 
 # Control Variables
-experimentVariantDatasetName = 'all-corrupted'
-resultsFileName = 'demo'
-modelName = 'demo-noise-' + experimentVariantDatasetName
-categoryOne = 'benign'
-categoryTwo = 'malignant'
-datasetPath = 'demo/demo-dataset-noise/'
-resultsPath = 'demo/demo-noise-results/'
-plotName = modelName
-graphSize = (15, 10)  # Size of result plots
-noEpochs = 10
-initialLearningRate = 1e-5
-batchSize = 32
+experimentVariantDatasetName = 'all-corrupted'  # Name of the experiment performed (used for graph titles etc.)
+resultsFileName = 'demo'    # Name of the result files
+modelName = 'demo-noise-' + experimentVariantDatasetName    # Name of the model used throughout graphs and results
+categoryOne = 'benign'  # Name of the first category for classification
+categoryTwo = 'malignant'   # Name of the second category for classification
+datasetPath = 'demo/demo-dataset-noise/'    # Path to the dataset to be used for classification
+resultsPath = 'demo/demo-noise-results/'    # Path to be used for output of graphs and statistics
+plotName = modelName    # Name of the graph (using model name)
+graphSize = (15, 10)  # Size of graph plots
+noEpochs = 100   # Number of epochs to run the model for (default 100)
+initialLearningRate = 1e-5  # Learning rate (determined from previous experimentation on dataset)
+batchSize = 32  # Size of sample batches to feed to the network
 decayRate = initialLearningRate / noEpochs
-numberOfClasses = 2
+numberOfClasses = 2 # Number of classification classes (two for binary classification)
 validationDatasetSize = 0.25  # Using 75% of the data for training and the remaining 25% for testing
 randomSeed = 42  # For repeatability
-imageHeight = 64
-imageWidth = 64
-imageDepth = 3
+imageHeight = 64    # Input image height
+imageWidth = 64     # Input image width
+imageDepth = 3      # Input image depth (three means Red, Green, Blue coloured image)
 
 
-# Determine whether given file is an image or not
+# Determine whether given file is an image
 def file_is_image(path_to_file):
     filename, extension = os.path.splitext(path_to_file)
     if extension != '.jpg':
@@ -74,7 +84,6 @@ def save_network_stats(resultsPath, modelName, history, fileName, sensitivity, s
     historyValLoss = str(historyValLoss[-1])  # Get last value from validated loss
     historyValAcc = history.history['val_acc']
     historyValAcc = str(historyValAcc[-1])  # Get last value from validated accuracy
-    # historyMSE = history.history['mse']
     historyMSE = 0  # str(historyMSE[-1])
     historyMAPE = 0  # history.history['mape']
     historyMAPE = 0  # str(historyMAPE[-1])
@@ -103,7 +112,6 @@ def build_network_model(width, height, depth, classes):
     if K.image_data_format() == 'channel_first':
         inputShape = (depth, height, width)
 
-    # Model Structure
     # First layer
     model.add(
         Conv2D(20, (5, 5), padding = "same", input_shape = inputShape))  # Learning 20 (5 x 5) convolution filters
@@ -117,12 +125,12 @@ def build_network_model(width, height, depth, classes):
 
     # Third layer - fully-connected layers
     model.add(Flatten())
-    model.add(Dense(50))  # 500 nodes
+    model.add(Dense(50))  # Originally 500 nodes, lowered to prevent overfitting
     model.add(Activation("relu"))
 
     # Softmax classifier
-    model.add(Dense(classes))  # number of nodes = number of classes
-    model.add(Activation("softmax"))  # yields probability for each class
+    model.add(Dense(classes))  # Number of nodes = number of classes
+    model.add(Activation("softmax"))  # Yields probability for each class
 
     # Return the model
     return model
@@ -131,14 +139,21 @@ def build_network_model(width, height, depth, classes):
 def load_dataset_subfolder(datasetSubfolderName):
     print(stamp() + "Classifying Dataset Subfolder for: " + datasetSubfolderName)
 
+    # Arrays used for storing the images and their corresponding labels
     imageArray = []
     labelArray = []
 
+    # Get path of the subfolder for a given dataset
     datasetSubfolderPath = datasetPath + '/' + experimentVariantDatasetName + '/' + datasetSubfolderName + '/'
+    
+    # Traverse the directory for a given dataset category in specified path
     for datasetCategory in os.listdir(datasetSubfolderPath):
+        # Save current path
         datasetCategoryPath = datasetSubfolderPath + '/' + datasetCategory
 
+        # Go through every image in the directory
         for imageSample in os.listdir(datasetCategoryPath):
+            # Only accept image files (ignores residual files)
             if file_is_image(datasetCategoryPath + '/' + imageSample):
                 # Load the image
                 image = cv2.imread(datasetCategoryPath + '/' + imageSample)
@@ -154,9 +169,12 @@ def load_dataset_subfolder(datasetSubfolderName):
                     label = 0
 
                 labelArray.append(label)
+
+    # Return collated images and their labels as arrays
     return imageArray, labelArray
 
 
+# Calculate confusion matrix statistics
 def calculate_statistics(tn, fp, fn, tp):
     sensitivity = tp / (tp + fn)
     specificity = tn / (fp + tn)
@@ -165,6 +183,7 @@ def calculate_statistics(tn, fp, fn, tp):
     return sensitivity, specificity, precision
 
 
+# Save the confusion matrix as a graphical figure
 def save_confusion_matrix(tp, tn, fp, fn):
     import seaborn as sns
     tp = int(tp)
@@ -215,31 +234,39 @@ trainingDatasetLabels = []
 validationDatasetImages = []
 validationDatasetLabels = []
 
+
+# Load the training images and labels
 (trainingDatasetImages, trainingDatasetLabels) = load_dataset_subfolder('train')
+
+# Load the validation images and labels
 (validationDatasetImages, validationDatasetLabels) = load_dataset_subfolder('validation')
 
+# Combine training images and labels and shuffle them randomly whilst maintaining their relationship
 trainingCombined = list(zip(trainingDatasetImages, trainingDatasetLabels))
 random.shuffle(trainingCombined)
 trainingDatasetImages[:], trainingDatasetLabels[:] = zip(*trainingCombined)
 
+# Combine validation images and labels and shuffle them randomly whilst maintaining their relationship
 validationCombined = list(zip(validationDatasetImages, validationDatasetLabels))
 random.shuffle(validationCombined)
 validationDatasetImages[:], validationDatasetLabels[:] = zip(*validationCombined)
 
-# Join validation and training datasets together (75% - 25% split)
+# Join validation and training (shuffled) datasets together
 combinedDatasetImages = trainingDatasetImages + validationDatasetImages
 combinedDatasetLabels = trainingDatasetLabels + validationDatasetLabels
 combinedDatasetImages = np.array(combinedDatasetImages, dtype = 'float') / 255.0
 combinedDatasetLabels = np.array(combinedDatasetLabels)
 
+# Print statistical information for verification purposes
 print(stamp() + 'Training Set Size: ' + str(len(trainingDatasetLabels)))
 print(stamp() + 'Validation Set Size: ' + str(len(validationDatasetLabels)))
 print(stamp() + 'Total Dataset Size: ' + str(len(combinedDatasetLabels)))
 
+# Split the datasets into training and testing subsets with a split determined by control variables (75% training default)
 (trainX, testX, trainY, testY) = train_test_split(combinedDatasetImages, combinedDatasetLabels,
                                                   test_size = validationDatasetSize, random_state = randomSeed)
 
-# Convert the labels from integers to vectors
+# Convert the labels to categorical type
 trainY = to_categorical(trainY, numberOfClasses)
 testY = to_categorical(testY, numberOfClasses)
 
@@ -248,14 +275,22 @@ aug = ImageDataGenerator()
 
 # Initialize the model
 print(stamp() + "Compiling Network Model")
+
+# Build the model based on control variable parameters
 model = build_network_model(width = imageWidth, height = imageHeight, depth = imageDepth, classes = numberOfClasses)
+
+# Set optimiser 
 opt = Adam(lr = initialLearningRate, decay = decayRate)
+
+# Compile the model using binary crossentropy, preset optimiser and selected metrics
 model.compile(loss = "binary_crossentropy",
               optimizer = opt,
               metrics = ["accuracy", "mean_squared_error", "mean_absolute_error"])
 
 # Train the network
 print(stamp() + "Training Network Model")
+
+# Save results of training in history dictionary for statistical analysis
 history = model.fit_generator(
     aug.flow(trainX, trainY, batch_size = batchSize),
     validation_data = (testX, testY),
@@ -263,11 +298,14 @@ history = model.fit_generator(
     epochs = noEpochs,
     verbose = 1)
 
+# The following can be used to produce confusion matrices if necessary
 # predictions = model.predict_classes(testX, batchSize, 0)
 # tn, fp, fn, tp = confusion_matrix(validationDatasetLabels, predictions).ravel()
 # print(tn, fp, fn, tp)
-sensitivity, specificity, precision = 0, 0, 0
+sensitivity, specificity, precision = 0, 0, 0   # Set to 0 if not used
 # sensitivity, specificity, precision = calculate_statistics(tn, fp, fn, tp)
+
+# Save all runtime statistics and plot graphs
 save_network_stats(resultsPath, modelName, history, resultsFileName, sensitivity, specificity, precision)
 # save_confusion_matrix(tn, fp, fn, tp)
 save_accuracy_graph(history)
@@ -279,5 +317,6 @@ model_json = model.to_json()
 with open(resultsPath + '/' + modelName + ".json", "w") as json_file:
     json_file.write(model_json)
 
+# Save weights to disk
 print(stamp() + "Saving Network Weights")
 model.save_weights(resultsPath + '/' + modelName + ".h5", "w")
