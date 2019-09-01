a3a7eebf0b6f87def16e377329f9c4095fa836e4
Jake <janiemiec20@gmail.com>
2019-09-01 23:14:51 +0200

    Extracted helper methods into a separate class file

---
+++ Jupyter/Binary-Classifier.py
@@ -0,0 +1,187 @@
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import img_to_array
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Flatten
+from keras.layers.core import Dense
+from keras import backend as K
+
+import numpy as np
+import random
+import cv2
+import os
+import classifierHelpers
+
+resultsFileName = 'Demo'
+datasetPath = 'Demo-dataset-rotation/'
+rotationRange = 135
+noEpochs = 100
+initialLearningRate = 1e-5
+batchSize = 32
+decayRate = initialLearningRate / noEpochs
+validationDatasetSize = 0.25
+randomSeed = 42
+imageDepth = 3
+
+resultsPath = 'Demo-results/'
+modelName = resultsFileName + "-" + str(rotationRange)
+plotName = modelName
+
+Tools = classifierHelpers.Helper(resultsPath, modelName)
+
+
+# Build the network structure
+def build_network_model(width, height, depth, classes):
+    # Initialise the model
+    model = Sequential()
+    inputShape = (height, width, depth)
+
+    # If 'channel first' is being used, update the input shape
+    if K.image_data_format() == 'channel_first':
+        inputShape = (depth, height, width)
+
+    # First layer
+    model.add(
+        Conv2D(20, (5, 5), padding="same", input_shape=inputShape))  # Learning 20 (5 x 5) convolution filters
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    # Second layer
+    model.add(Conv2D(50, (5, 5), padding="same"))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    # Third layer - fully-connected layers
+    model.add(Flatten())
+    model.add(Dense(50))  # 500 nodes
+    model.add(Activation("relu"))
+
+    # Softmax classifier
+    model.add(Dense(classes))  # number of nodes = number of classes
+    model.add(Activation("softmax"))  # yields probability for each class
+
+    # Return the model
+    return model
+
+
+
+
+# Initialize the data and labels arrays
+sortedData = []
+sortedLabels = []
+data = []
+labels = []
+
+# Go through dataset directory
+print(Tools.stamp() + "Classifying the Dataset")
+for datasetCategory in os.listdir(datasetPath):
+    datasetCategoryPath = datasetPath + "/" + datasetCategory
+
+    # Go through category 1 and then category 2 of the dataset
+    for sample in os.listdir(datasetCategoryPath):
+        # print(stamp() + sample)
+        if Tools.file_is_image(datasetCategoryPath + "/" + sample):
+            image = cv2.imread(datasetCategoryPath + "/" + sample)
+            image = cv2.resize(image, (
+                64, 64))
+            image = img_to_array(image)
+            # Save image to the data list
+            sortedData.append(image)
+
+            # Decide on binary label
+            if datasetCategory == 'benign':
+                label = 1
+            else:
+                label = 0
+            # Save label for the current image
+            sortedLabels.append(label)
+
+combined = list(zip(sortedData, sortedLabels))
+random.shuffle(combined)
+data[:], labels[:] = zip(*combined)
+
+# Scale the raw pixel intensities to the range [0, 1]
+data = np.array(data, dtype="float") / 255.0
+labels = np.array(labels)
+
+validationDatasetLabels = []
+# testSet = 0.25 * len(labels)
+validationDatasetLabels = labels[-7:]
+
+# Partition the data into training and testing splits
+(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=7,  # Set manually to 7 for Demo
+                                                  random_state=randomSeed)
+
+# Convert the labels from integers to vectors
+trainY = to_categorical(trainY, num_classes=2)
+testY = to_categorical(testY, num_classes=2)
+
+# Construct the image generator for data augmentation
+aug = ImageDataGenerator(
+    rotation_range=rotationRange,
+    fill_mode="nearest"
+)
+
+augValidation = ImageDataGenerator(
+    rotation_range=rotationRange,
+    fill_mode="nearest"
+)
+
+# Initialize the model
+print(Tools.stamp() + "Compiling Network Model")
+
+# Build the model based on control variable parameters
+model = build_network_model(
+    width=64, height=64, depth=imageDepth, classes=3)
+
+# Set optimiser
+opt = Adam(lr=initialLearningRate, decay=decayRate)
+
+# Compile the model using binary crossentropy, preset optimiser and selected metrics
+model.compile(loss="binary_crossentropy",
+              optimizer=opt,
+              metrics=["accuracy", "mean_squared_error", "mean_absolute_error"])
+# Train the network
+print(Tools.stamp() + "Training Network Model")
+
+# Save results of training in history dictionary for statistical analysis
+history = model.fit_generator(
+    aug.flow(trainX, trainY, batch_size=batchSize),
+    validation_data=(testX, testY),
+    steps_per_epoch=len(trainX) // batchSize,
+    epochs=noEpochs,
+    verbose=1)
+
+# The following can be used to produce confusion matrices if necessary
+# predictions = model.predict_classes(testX, batchSize, 0)
+# tn, fp, fn, tp = confusion_matrix(validationDatasetLabels, predictions).ravel()
+# print(tn, fp, fn, tp)
+sensitivity, specificity, precision = 0, 0, 0   # Set to 0 if not used
+# sensitivity, specificity, precision = calculate_statistics(tn, fp, fn, tp)
+
+# Save all runtime statistics and plot graphs
+Tools.save_network_stats(resultsPath, modelName, history,
+                   resultsFileName, sensitivity, specificity, precision)
+# save_confusion_matrix(tn, fp, fn, tp)
+Tools.save_accuracy_graph(history)
+Tools.save_loss_graph(history)
+
+# Save the model to disk
+print(Tools.stamp() + "Saving Network Model")
+model_json = model.to_json()
+with open(resultsPath + '/' + modelName + ".json", "w") as json_file:
+    json_file.write(model_json)
+
+# Save weights to disk
+print(Tools.stamp() + "Saving Network Weights")
+model.save_weights(resultsPath + '/' + modelName + ".h5", "w")
+
+
+
---
+++ Jupyter/classifierHelpers.py
@@ -0,0 +1,108 @@
+import datetime
+import matplotlib
+import matplotlib.pyplot as plt
+import os
+
+
+class Helper(object):
+	graphSize = (15, 10)
+
+	def __init__(self, resultsPath, modelName):
+		self.resultsPath = resultsPath
+		self.modelName = modelName
+
+	def file_is_image(self, path_to_file):
+		filename, extension = os.path.splitext(path_to_file)
+		if extension != '.jpg':
+			return False
+		else:
+			return True
+
+	# Prints current timestamp, to be used in print statements
+	def stamp(self):
+		time = "[" + str(datetime.datetime.now().time()) + "]   "
+		return time
+
+	# Save final model performance
+	def save_network_stats(self, resultsPath, modelName, history, fileName, sensitivity, specificity, precision, noEpochs,
+	                       initialLearningRate):
+		# Extract data from history dictionary
+		historyLoss = history.history['loss']
+		historyLoss = str(historyLoss[-1])  # Get last value from loss
+		historyAcc = history.history['acc']
+		historyAcc = str(historyAcc[-1])  # Get last value from accuracy
+		historyValLoss = history.history['val_loss']
+		# Get last value from validated loss
+		historyValLoss = str(historyValLoss[-1])
+		historyValAcc = history.history['val_acc']
+		# Get last value from validated accuracy
+		historyValAcc = str(historyValAcc[-1])
+		historyMSE = 0  # str(historyMSE[-1])
+		historyMAPE = 0  # history.history['mape']
+		historyMAPE = 0  # str(historyMAPE[-1])
+
+		with open(resultsPath + fileName + ".txt", "a") as history_log:
+			history_log.write(
+				modelName + "," + historyLoss + "," + historyAcc + "," + historyValLoss + "," + historyValAcc + "," + str(
+					noEpochs) + "," + str(initialLearningRate) + "," + str(historyMSE) + "," + str(
+					historyMAPE) + "," + str(sensitivity) + "," + str(specificity) + "," + str(precision) + "\n")
+		history_log.close()
+
+		print(self.stamp() + "Keras Log Saved")
+
+		print(history.history.keys())
+
+		print(self.stamp() + "History File Saved")
+
+	# Calculate confusion matrix statistics
+	def calculate_statistics(self, tn, fp, fn, tp):
+		sensitivity = tp / (tp + fn)
+		specificity = tn / (fp + tn)
+		precision = tp / (tp + fp)
+
+		return sensitivity, specificity, precision
+
+	def save_figure(self, fig, figureName):
+		fig.savefig(self.resultsPath + '/' + self.modelName + '-' + figureName + '.png')
+
+	# Save the confusion matrix as a graphical figure
+	def save_confusion_matrix(self, tp, tn, fp, fn, resultsPath, modelName):
+		import seaborn as sns
+		tp = int(tp)
+		tn = int(tn)
+		fp = int(fp)
+		fn = int(fn)
+
+		cm = [[tp, tn], [fp, fn]]
+		cm = np.array(cm)
+		heatmap = sns.heatmap(cm, annot = True, fmt = 'g', linewidths = 0.2)
+		fig = heatmap.get_figure()
+		self.save_figure(fig, 'confusion-matrix')
+
+	# Summarize history for accuracy
+	def save_accuracy_graph(self,history, modelName):
+		plt.figure(figsize = self.graphSize, dpi = 75)
+		plt.grid(True, which = 'both')
+		plt.plot(history.history['acc'])
+		plt.plot(history.history['val_acc'])
+		plt.title('Model Accuracy')
+		plt.ylabel('accuracy')
+		plt.xlabel('epoch')
+		plt.legend(['train', 'test'], loc = 'upper left')
+		plt.suptitle(modelName)
+		self.save_figure(plt, 'accuracy')
+		plt.close()
+
+	# Summarize history for loss
+	def save_loss_graph(self,history, modelName):
+		plt.figure(figsize = self.graphSize, dpi = 75)
+		plt.grid(True, which = 'both')
+		plt.plot(history.history['loss'])
+		plt.plot(history.history['val_loss'])
+		plt.title('Model Loss')
+		plt.ylabel('loss')
+		plt.xlabel('epoch')
+		plt.legend(['train', 'test'], loc = 'upper left')
+		plt.suptitle(modelName)
+		self.save_figure(plt, 'loss')
+		plt.close()
