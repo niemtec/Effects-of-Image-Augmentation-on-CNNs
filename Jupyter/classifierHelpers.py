6c628e271329bf949cfa07c45145f7a7b4c3697b
Jake <janiemiec20@gmail.com>
2019-09-02 09:12:34 +0200

    Renamed more variables to follow convention

--- Jupyter/classifierHelpers.py
+++ Jupyter/classifierHelpers.py
@@ -4,11 +4,11 @@ import os
 
 
 class Helper(object):
-	graphSize = (15, 10)
+	graph_size = (15, 10)
 
-	def __init__(self, resultsPath, modelName):
-		self.resultsPath = resultsPath
-		self.modelName = modelName
+	def __init__(self, results_path, model_name):
+		self.results_path = results_path
+		self.model_name = model_name
 
 	# Prints current timestamp, to be used in print statements
 	def stamp(self):
@@ -16,33 +16,33 @@ class Helper(object):
 		return time
 
 	# Save final model performance
-	def saveNetworkStats(self, history, noEpochs, initialLearningRate):
+	def saveNetworkStats(self, history, epochs, initial_learning_rate):
 		# Extract data from history dictionary
-		historyLoss = history.history['loss']
-		historyLoss = str(historyLoss[-1])  # Get last value from loss
-		historyAcc = history.history['acc']
-		historyAcc = str(historyAcc[-1])  # Get last value from accuracy
-		historyValLoss = history.history['val_loss']
-		historyValLoss = str(historyValLoss[-1])
-		historyValAcc = history.history['val_acc']
-		historyValAcc = str(historyValAcc[-1])
-		historyMSE = 0  # str(historyMSE[-1])
+		history_loss = history.history['loss']
+		history_loss = str(history_loss[-1])  # Get last value from loss
+		history_acc = history.history['acc']
+		history_acc = str(history_acc[-1])  # Get last value from accuracy
+		history_val_loss = history.history['val_loss']
+		history_val_loss = str(history_val_loss[-1])
+		history_val_acc = history.history['val_acc']
+		history_val_acc = str(history_val_acc[-1])
+		history_mse = str(history.history['mse'])
 
-		with open(self.resultsPath + self.modelName + ".txt", "a") as history_log:
+		with open(self.results_path + self.model_name + ".txt", "a") as history_log:
 			history_log.write(
-				self.modelName + "," + historyLoss + "," + historyAcc + "," + historyValLoss + "," + historyValAcc + "," + str(noEpochs) + "," + str(initialLearningRate) + "," + str(historyMSE) + "\n")
+				self.model_name + "," + history_loss + "," + history_acc + "," + history_val_loss + "," + history_val_acc + "," + str(epochs) + "," + str(initial_learning_rate) + "," + str(history_mse) + "\n")
 		history_log.close()
 
 		print(self.stamp() + "Keras Log Saved")
 		print(history.history.keys())
 		print(self.stamp() + "History File Saved")
 
-	def saveFigureStats(self, fig, figureName):
-		fig.savefig(self.resultsPath + '/' + self.modelName + '-' + figureName + '.png')
+	def saveFigureStats(self, fig, figure_name):
+		fig.savefig(self.results_path + '/' + self.model_name + '-' + figure_name + '.png')
 
 	# Summarize history for accuracy
 	def saveAccuracyGraph(self, history):
-		plt.figure(figsize = self.graphSize, dpi = 75)
+		plt.figure(figsize = self.graph_size, dpi = 75)
 		plt.grid(True, which = 'both')
 		plt.plot(history.history['acc'])
 		plt.plot(history.history['val_acc'])
@@ -50,13 +50,13 @@ class Helper(object):
 		plt.ylabel('accuracy')
 		plt.xlabel('epoch')
 		plt.legend(['train', 'test'], loc = 'upper left')
-		plt.suptitle(self.modelName)
+		plt.suptitle(self.model_name)
 		self.saveFigureStats(plt, 'accuracy')
 		plt.close()
 
 	# Summarize history for loss
 	def saveLossGraph(self, history):
-		plt.figure(figsize = self.graphSize, dpi = 75)
+		plt.figure(figsize = self.graph_size, dpi = 75)
 		plt.grid(True, which = 'both')
 		plt.plot(history.history['loss'])
 		plt.plot(history.history['val_loss'])
@@ -64,19 +64,19 @@ class Helper(object):
 		plt.ylabel('loss')
 		plt.xlabel('epoch')
 		plt.legend(['train', 'test'], loc = 'upper left')
-		plt.suptitle(self.modelName)
+		plt.suptitle(self.model_name)
 		self.saveFigureStats(plt, 'loss')
 		plt.close()
 	
 	def saveModelToDisk(self, model):
 		print(self.stamp() + "Saving Network Model")
 		model_json = model.to_json()
-		with open(self.resultsPath + '/' + self.modelName + ".json", "w") as json_file:
+		with open(self.results_path + '/' + self.model_name + ".json", "w") as json_file:
 			json_file.write(model_json)
 		
 	def saveWeightsToDisk(self, model):
 		print(self.stamp() + "Saving Network Weights")
-		model.save_weights(self.resultsPath + '/' + self.modelName + ".h5", "w")
+		model.save_weights(self.results_path + '/' + self.model_name + ".h5", "w")
 	
 	@staticmethod
 	def isFileAnImage(path_to_file):
