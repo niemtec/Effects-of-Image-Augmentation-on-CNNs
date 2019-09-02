63d788def82549048f24f8081469167518bb7ab0
Jake <janiemiec20@gmail.com>
2019-09-02 11:09:29 +0200

    Created dataset classifier to extract dataset into an array form for faster loading in the future

---
+++ Jupyter/datasetClassifier.py
@@ -0,0 +1,41 @@
+
+from keras.preprocessing.image import img_to_array
+import numpy as np
+import cv2
+import os
+from classifierHelpers import Helper
+
+Tools = Helper(None, None)
+
+dataset_path = '../Cancer-Dataset/'
+
+# Initialize the data and labels arrays
+sorted_data = []
+sorted_labels = []
+data = []
+labels = []
+
+# Go through dataset directory
+print(Tools.stamp() + "Classifying the Dataset")
+for dataset_category in os.listdir(dataset_path):
+	dataset_category_path = dataset_path + "/" + dataset_category
+	
+	if not dataset_category.startswith('.'):
+		for sample in os.listdir(dataset_category_path):
+			print(Tools.stamp() + sample)
+			
+			if Tools.isFileAnImage(dataset_category_path + "/" + sample):
+				image = cv2.imread(dataset_category_path + "/" + sample)
+				image = cv2.resize(image, (64, 64))
+				image = img_to_array(image)
+				sorted_data.append(image)
+				
+				if dataset_category == 'benign':
+					sorted_labels.append(1)
+				else:
+					sorted_labels.append(0)
+					
+#TODO SAVE THE ARRAY INTO A FILE FOR LATER LOADING
+np.save('sorted_data_array.txt', sorted_data)
+np.save('sorted_labels_array', sorted_labels)
+print('Saving complete')
